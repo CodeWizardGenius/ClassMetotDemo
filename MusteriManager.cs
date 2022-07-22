3fcf96c2044c83d0319f24aa7c8b88d83ec5b530
BarisKarapelit <baris_karapelit_298@hotmail.com>
2022-07-22 14:48:55 +0300

    Menu class adding

--- MusteriManager.cs
+++ MusteriManager.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ClassMetotDemo
 {
     public class MusteriManager
     {
-        
+        private List<Musteri> musteriListesi; 
         public void Ekleme()
         {
-          
+            int adet= 0;
            Console.WriteLine("Kaç Adet Müşteri Eklenecek?");
-           int adet = Convert.ToInt32(Console.ReadLine());
-           Musteri[]  musteriler = new Musteri[adet];
+            adet= Convert.ToInt32(Console.ReadLine());
+           musteriListesi=Enumerable.Range(0, adet).Select(_ => new Musteri()).ToList();
            for (int i = 0; i < adet; i++)
            {
                Console.WriteLine("Müşteri Adı:");
-               musteriler[i].Name=Console.ReadLine();
+               musteriListesi[i].Name=Console.ReadLine();
                Console.WriteLine("Müşteri Soyadı:");
-               musteriler[i].SurName = Console.ReadLine();
                Console.WriteLine("Müşteri Cinsiyeti:");
                char ch;
                char.TryParse(Console.ReadLine(), out ch);
-               musteriler[i].Gender = ch;
+               musteriListesi[i].Gender = ch;
                Console.WriteLine("Enter a date: ");
                DateTime userDateTime;
                if (DateTime.TryParse(Console.ReadLine(), out userDateTime))
@@ -31,38 +32,38 @@ namespace ClassMetotDemo
                {
                    Console.WriteLine("You have entered an incorrect value");
                }
-               musteriler[i].BirthDate=userDateTime;
+               musteriListesi[i].BirthDate=userDateTime;
                Console.WriteLine("Medeni Hali:");
-                musteriler[i].MaritalStatus = Console.ReadLine();
+                musteriListesi[i].MaritalStatus = Console.ReadLine();
                 Console.WriteLine("Yaşadınız Şehir:");
-                musteriler[i].City = Console.ReadLine();
+                musteriListesi[i].City = Console.ReadLine();
                 Console.WriteLine("Yaşadınız ülke:");
-                musteriler[i].Country = Console.ReadLine();
+                musteriListesi[i].Country = Console.ReadLine();
                 Console.WriteLine("Müşteri Telefon Numarası:");
-                musteriler[i].Phone = Console.ReadLine();
+                musteriListesi[i].Phone = Console.ReadLine();
                 Console.WriteLine("Müşteri E-Posta Adresi:");
-                musteriler[i].Email = Console.ReadLine();
+                musteriListesi[i].Email = Console.ReadLine();
                 
 
            }
         }
 
-        // public void Listeleme()
-        // {
-        //     foreach (var item in musteriler)
-        //     {
-        //         Console.WriteLine("Müşteri Adı:" + item.Name);
-        //         Console.WriteLine("Müşteri Soyadı:" + item.SurName);
-        //         Console.WriteLine("Müsteri Cinsiyeti: " + item.Gender);
-        //         Console.WriteLine("Müsteri Doğum Tarihi:" + item.BirthDate);
-        //         Console.WriteLine("Müsteri Medeni Hali:" + item.MaritalStatus);
-        //         Console.WriteLine("Müsteri Yaşadığı Şehir:" + item.City);
-        //         Console.WriteLine("Müsteri Yaşadığı Ülke:" + item.Country);
-        //         Console.WriteLine("Müsteri Telefon Numarası:" + item.Phone);
-        //         Console.WriteLine("Müsteri E-Posta Adresi:" + item.Email);
-        //         Console.WriteLine("-----------------------------------------------------");
-        //     }
-        // }
+        public void Listeleme()
+        {
+            foreach (var item in musteriListesi )
+            {
+                Console.WriteLine("Müşteri Adı:" + item.Name);
+                Console.WriteLine("Müşteri Soyadı:" + item.SurName);
+                Console.WriteLine("Müsteri Cinsiyeti: " + item.Gender);
+                Console.WriteLine("Müsteri Doğum Tarihi:" + item.BirthDate);
+                Console.WriteLine("Müsteri Medeni Hali:" + item.MaritalStatus);
+                Console.WriteLine("Müsteri Yaşadığı Şehir:" + item.City);
+                Console.WriteLine("Müsteri Yaşadığı Ülke:" + item.Country);
+                Console.WriteLine("Müsteri Telefon Numarası:" + item.Phone);
+                Console.WriteLine("Müsteri E-Posta Adresi:" + item.Email);
+                Console.WriteLine("-----------------------------------------------------");
+            }
+        }
 
         public void Silme()
         {
--- Program.cs
+++ Program.cs
@@ -6,39 +6,8 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            MusteriManager musteriManager = new MusteriManager();
-            string programHeader = "Banka Müsteri Takip Sistemi v1.0";
-            string programFooter = "Programın son haliyle kullanıldığınızdan teşekkür ederiz."; 
-            string programMenu = "1-Müşteri Ekle\n2-Müşteri Listele\n3-Müşteri Sil\n4-Müşteri Güncelle\n5-Çıkış\nSeçiniz: ";
-            
-            Console.SetCursorPosition((Console.WindowWidth - programHeader.Length) / 2, Console.CursorTop);
-            Console.WriteLine(programHeader);
-            Console.Write(programMenu);
-            int menu=Convert.ToInt16(Console.ReadLine());
-            switch (menu) 
-            {
-                case 1:
-                    musteriManager.Ekleme();
-                    break;
-                case 2:
-                    // musteriManager.Listeleme();
-                    break;
-                case 3:
-                    musteriManager.Silme();
-                    break;
-                case 4:
-                    Console.WriteLine("Güncelleme işlemi yapılacak");
-                    break;
-                case 5:
-                    Console.WriteLine(programFooter);
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Geçersiz seçim");
-                    
-                    Main(args);
-                    break;
-            }
+            Menu menu = new Menu();
+            Menu.Menus();
         }
     }
 }
